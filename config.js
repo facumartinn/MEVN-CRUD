f8eac5932ad073a6dc5ce5b2b56330e71b74192f
facumartinn <facumartinn@gmail.com>
2020-12-04 14:07:21 -0300

    Finished edit and delete option

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: process.env.MONGO_URI,
+    mongoUri: 'mongodb+srv://facudb:Mdpcteamoo1.@facudb.xtev6.mongodb.net/facudb?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
 }
